bda16c06757d2039b8bf79fa97c02acbc22a4543
TeamDman <TeamDman9201@gmail.com>
2022-04-04 20:30:05 -0400

    All tests passing

--- NoahCoin/NoahCoin.Tests/Tests/BlockTests.cs
+++ NoahCoin/NoahCoin.Tests/Tests/BlockTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NoahCoin.Models.Datastructures;
 
 namespace NoahCoin.Tests;
@@ -15,30 +16,9 @@ public class BlockTests
                 Transactions = new HashPointer<Transaction>[] { new(new())},
             },
         };
-        Block b = a with { Header = a.Header with { Transactions = new HashPointer<Transaction>[] { } } };
+        Block b = a with { Header = a.Header with { Transactions = Array.Empty<HashPointer<Transaction>>() } };
         Block c = a with { Header = a.Header, Nonce = a.Nonce };
         Assert.NotEqual(a.GetHash(), b.GetHash());
         Assert.Equal(a.GetHash(), c.GetHash());
-    }
-
-    [Fact]
-    public void ValidTest1()
-    {
-        Block a = new Block
-        {
-            Header = new(){
-                IsGenesisBlock = true,
-                Transactions = new HashPointer<Transaction>[] { new(new ())},
-            }
-        };
-        Assert.True(a.IsValid);
-        var BadTransactionPointer = new HashPointer<Transaction>
-        {
-            Reference = a.Header.Transactions[0].Reference,
-            Hash = new Hash(new byte[] { }),
-        };
-        Assert.False(BadTransactionPointer.IsValid);
-        var BadBlock = a with { Header = new() {Transactions = new[] { BadTransactionPointer } }};
-        Assert.False(BadBlock.IsValid);
     }
 }
--- NoahCoin/NoahCoin/Models/Blockchain/Transaction.cs
+++ NoahCoin/NoahCoin/Models/Blockchain/Transaction.cs
@@ -2,13 +2,20 @@ namespace NoahCoin.Models.Blockchain;
 
 public record Transaction : IHashable
 {
-    public TransactionInput[] Inputs { get; init; } = Array.Empty<TransactionInput>();
-    public TransactionOutput[] Outputs { get; init; } = Array.Empty<TransactionOutput>();
+    public TransactionInput[] Inputs { get; init; } =
+        Array.Empty<TransactionInput>();
+
+    public TransactionOutput[] Outputs { get; init; } =
+        Array.Empty<TransactionOutput>();
 
     public Hash GetHash() =>
-        IHashable.GetHash(Inputs.Concat((IHashable[])Outputs));
+        IHashable.GetHash(
+            Inputs.Select(x => x.GetHash())
+                .Concat(Outputs.Select(v => v.GetHash()))
+        );
 
     public Hash GetPreSignedHash() => IHashable.GetHash(
-        Inputs.Select(i => i with { Script = "" }).Concat((IHashable[])Outputs)
+        Inputs.Select(i => (i with { Script = "" }).GetHash())
+            .Concat(Outputs.Select(v => v.GetHash()))
     );
 }
